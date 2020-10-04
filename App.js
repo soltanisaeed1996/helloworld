742bf0b2d0e5b503c2d655047a9d216fee396a25
saeed soltani <soltanisaeed1996@gmail.com>
2020-10-04 19:41:51 +0330

    finish this test app

--- App.js
+++ App.js
@@ -6,7 +6,14 @@
  * @flow strict-local
  */
 import React, {useState} from 'react';
-import {Text, TextInput, View, Button} from 'react-native';
+import {
+  Text,
+  TextInput,
+  View,
+  Button,
+  ScrollView,
+  ImageBackground,
+} from 'react-native';
 
 const pizzaholder = (props) => {
   const [name, setName] = useState('');
@@ -14,28 +21,41 @@ const pizzaholder = (props) => {
 
   return (
     <View style={{padding: 10}}>
-      <TextInput
-        style={{height: 40}}
-        placeholder="please type your name"
-        value={name}
-        onChangeText={(x) => setName(x)}
-      />
-      <TextInput
-        style={{height: 40}}
-        placeholder="please type your name"
-        value={family}
-        onChangeText={(x) => setFamily(x)}
-      />
-      <Text>
-        hello {name} {family}
-      </Text>
-      <Button
-        onPress={() => {
-          setFamily('');
-          setName('');
-        }}
-        title="reset"
-      />
+      <ImageBackground style={{alignSelf: 'stretch'}}>
+        <ScrollView>
+          <TextInput
+            style={{height: 40}}
+            placeholder="please type your name"
+            value={name}
+            onChangeText={(x) => setName(x)}
+          />
+          <TextInput
+            style={{height: 40}}
+            placeholder="please type your name"
+            value={family}
+            onChangeText={(x) => setFamily(x)}
+          />
+          <Text>
+            hello {name} {family}
+          </Text>
+          <Button
+            onPress={() => {
+              setFamily('');
+              setName('');
+            }}
+            title="reset"
+          />
+          <Text style={{fontSize: 45}}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Text>
+        </ScrollView>
+      </ImageBackground>
     </View>
   );
 };
